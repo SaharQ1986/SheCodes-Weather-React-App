922561aeb90f81762a4139596b837f7c17ec314b
Sahar Qasemi <sahar.gh9244@gmail.com>
2023-06-04 02:17:55 +0430

    add github repo address

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>Weather App</h2>
+      Weather App
+      <a
+        href="https://github.com/SaharQ1986/SheCodes-Weather-React-App.git"
+        target="_blank"
+      ></a>
     </div>
   );
 }

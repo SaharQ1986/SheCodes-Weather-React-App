fdd2abe84a45a298eb52f44a2eb242c1f484a27a
Sahar Qasemi <sahar.gh9244@gmail.com>
2023-06-06 00:28:48 +0430

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Weather App
-      <a
-        rel="noreferrer"
-        href="https://github.com/SaharQ1986/SheCodes-Weather-React-App.git"
-        target="_blank"
-      ></a>
+      <div className="container">
+        Weather App
+        <a
+          rel="noreferrer"
+          href="https://github.com/SaharQ1986/SheCodes-Weather-React-App.git"
+          target="_blank"
+        ></a>
+      </div>
     </div>
   );
 }

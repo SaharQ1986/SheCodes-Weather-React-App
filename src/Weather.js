47a4a5acc04818c76f6f71b4d33f1186d459ae4d
Sahar Qasemi <sahar.gh9244@gmail.com>
2023-06-06 01:51:44 +0430

    add initial css styles

--- src/Weather.js
+++ src/Weather.js
@@ -12,10 +12,15 @@ function Weather() {
               type="search"
               placeholder="Enter a city ..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-4">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -24,13 +29,17 @@ function Weather() {
         <li>Wednesday 11:20</li>
         <li>Scattered showers</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png"
-            alt="Scattered showers"
-          />
-          18 °C
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png"
+              alt="Scattered showers"
+              className="float-left"
+            />
+            <span className="temp">18</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>

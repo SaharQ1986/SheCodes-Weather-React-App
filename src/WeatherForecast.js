0c19767030895abd25c09282642551920b3a95a8
Sahar Qasemi <sahar.gh9244@gmail.com>
2023-06-16 07:44:55 +0430

    add new component for forecast Info

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,36 +1,38 @@
-import React from "react";
-
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./WeatherForecast.css";
 import "./Weather.css";
 import axios from "axios";
+import WeatherForecastInfo from "./WeatherForecastInfo";
 
 function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState("");
   function forecastResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
-  console.log(props);
-  const apiKey = "866a208a73eeff02182218e9441647a1";
-  let long = props.coordinates.lon;
-  let lat = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units={metric}`;
-  axios.get(apiUrl).then(forecastResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="dailyforecast opacity-75">Mon</div>
-          <WeatherIcon code="01n" className="daily-icon" size={36} />
-          <div className="daily-temp">
-            <span className="daily-temp-max">22°</span>
-            <span className="daily-temp-min">10° </span>
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastInfo data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "866a208a73eeff02182218e9441647a1";
+    let long = props.coordinates.lon;
+    let lat = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(forecastResponse);
+
+    return null;
+  }
 }
 
 export default WeatherForecast;
---
+++ src/WeatherForecastInfo.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import WeatherIcon from "./WeatherIcon";
+
+function WeatherForecastInfo(props) {
+  let maxTemp = Math.round(props.data.temp.max);
+  let minTemp = Math.round(props.data.temp.min);
+  let icon = props.data.weather[0].icon;
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    return days[day];
+  }
+  return (
+    <div>
+      <div className="dailyforecast opacity-75">{day()}</div>
+      <WeatherIcon code={icon} className="daily-icon" size={36} />
+      <div className="daily-temp">
+        <span className="daily-temp-max">{maxTemp}°</span>
+        <span className="daily-temp-min">{minTemp}° </span>
+      </div>
+    </div>
+  );
+}
+
+export default WeatherForecastInfo;
